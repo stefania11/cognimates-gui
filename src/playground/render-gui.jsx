97af66cf3820049b539a319522248811c09c6383
satvu <sarahvu@mit.edu>
2019-03-16 10:01:03 -0400

    add link to cognimates main site

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -9,7 +9,7 @@ import TitledHOC from '../lib/titled-hoc.jsx';
 import log from '../lib/log.js';
 
 const onClickLogo = () => {
-    window.location = 'https://scratch.mit.edu';
+    window.location = 'https://mitmedialab.github.io/cognimates-website/about/';
 };
 
 const handleTelemetryModalCancel = () => {
