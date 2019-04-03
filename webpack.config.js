1503cd3178857ff799617a29469bebeff85c80a1
tqiu8 <tammy.qiu@yahoo.com>
2019-04-03 11:11:43 -0400

    gui translation wip

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ var postcssImport = require('postcss-import');
 
 const base = {
     mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    node: {'fs': 'empty'},
     devtool: 'cheap-module-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
